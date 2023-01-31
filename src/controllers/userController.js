b0ecfc78e183d66b1bc5f1aa6e9dc33df9035a21
tonildo123 <tony81191@gmail.com>
2023-01-31 10:04:52 -0300

    update create user

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -2,20 +2,34 @@ const User = require('../models/user');
 
 const createUser = async (req, res)=>{
 
-    const user = User(req.body)
-    await user.save()
-    .then((user)=>{
+    const {email, password} = User(req.body)
+    const user =await User.findOne({email})
+
+    try {
+
+        if(user){
+            return res.status(400).json({
+                message: "User already exists",
+                data:user
+            })
+        } 
+        const newUSer = new User({email, password})
+        await newUSer.save()
         res.status(200).json({
             message: "User created",
-            data:user
+            data:newUSer
         })
-    })
-    .catch((err)=>{
+
+    } catch (error) {
+
         res.status(500).json({
             message: "Error creating user",
-            error: err
+            error: error
         })
-    })
+        console.log('error al crear usuario', error)
+        
+    }
+    
 }
 
 const getAllUsers = async (req, res)=>{
