15fea6fc38297781d3bc46830c49ddb1460b0a7f
tonildo123 <tony81191@gmail.com>
2023-07-09 17:59:27 -0300

    actualizacion de la conexion a mongo db

---
+++ README.md
@@ -0,0 +1,14 @@
+## comando para crear proyecto backend
+npm init -y
+## luego instalaciones 
+### / libreria de node js para crear servidores
+npm install express 
+### / libreria para escuchar cambios en la app
+npm install nodemon 
+### middleware
+bloque de codigo que se ejecuta entre la request y el servidor
+ejemplo app.use(express.json()) diciendo que en la request envio
+un objeto json
+### morgan otro middleware 
+identifica el tipo de request y es necesario importarlo con el require
+
--- src/index.js
+++ src/index.js
@@ -1,26 +1,29 @@
 const express = require('express');
 const mongoose = require('mongoose')
+const morgan = require('morgan')
 require('dotenv').config()
 const allRouter = require('./routes/index')
 
 const app = express();
+
 const port = process.env.PORT || 3636;
 
 app.use(express.json()) // This is the middleware that allows us to parse the body of the request
-app.use(express.urlencoded({extended: false}));
+app.use(express.urlencoded({ extended: false }));
+app.use(morgan('dev'))
 app.use("/api", allRouter) // This is the base path for all routes
-app.get("/", (req, res)=>{
+app.get("/", (req, res) => {
 
     res.send("Bienvenido a OH MY DOG")
 
 })
 
 
-// mongoose.set('strictQuery', true);
+mongoose.set('strictQuery', true);
 mongoose
-.connect(process.env.MONGODB_URI)
-.then(()=>{console.log('coneected to mongo db')})
-.catch((err)=>{console.log(err)})
+    .connect(process.env.MONGODB_URI)
+    .then(() => { console.log('coneected to mongo db') })
+    .catch((err) => { console.log(err) })
 
 
 app.listen(port, () => {
